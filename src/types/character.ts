be1ed1a90327880c4b9ee3663afa328b6f54d8be
Abhay Khanpara <abhaykhanapara.dev@gmail.com>
2024-05-23 10:44:08 +0530

    feat:changes

--- src/types/character.ts
+++ src/types/character.ts
@@ -1,27 +1,3 @@
-// // types/character.ts
-// export interface Character {
-//   id: number;
-//   name: string;
-//   status: string;
-//   species: string;
-//   type: string;
-//   gender: string;
-//   origin: {
-//     name: string;
-//     url: string;
-//   };
-//   location: {
-//     name: string;
-//     url: string;
-//   };
-//   image: string;
-//   episode: string[];
-//   url: string;
-//   created: string;
-// }
-
-
-
 export interface Info {
   count: number;
   pages: number;
